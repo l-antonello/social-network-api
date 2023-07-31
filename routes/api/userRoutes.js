742ed911ec39bec61cebc8fefa34b09b486ac3af
l-antonello <122548483+l-antonello@users.noreply.github.com>
2023-07-31 10:43:21 -0400

    Update userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,15 +9,11 @@ const {
   removeFriend,
 } = require("../../controllers/userController");
 
-
 router.route("/").get(getUsers).post(createUser);
-
 
 router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
-
 router.route("/:userId/friends").post(addFriend);
-
 
 router.route("/:userId/friends/:friendId").delete(removeFriend);
 

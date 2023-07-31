968d83c316f3dfb98332cd43b8afbc92edfa7ddc
l-antonello <122548483+l-antonello@users.noreply.github.com>
2023-07-31 17:33:06 -0400

    Update userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,18 +3,25 @@ const {
   getUsers,
   getSingleUser,
   createUser,
-  updateUser,
+  // updateUser,
   deleteUser,
-  addFriend,
-  removeFriend,
+  // addFriend,
+  // removeFriend,
+  addReaction,
+  removeReaction,
 } = require("../../controllers/userController");
 
 router.route("/").get(getUsers).post(createUser);
 
-router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route("/:userId").get(getSingleUser).delete(deleteUser);
 
-router.route("/:userId/friends").post(addFriend);
+// router.route("/:userId/friends").post(addFriend);
 
-router.route("/:userId/friends/:friendId").delete(removeFriend);
+// router.route("/:userId/friends/:friendId").delete(removeFriend);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
 
 module.exports = router;

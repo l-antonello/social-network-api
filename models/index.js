7dc884e4a17162b5bf3ae40a932df2ed696e9d8a
l-antonello <122548483+l-antonello@users.noreply.github.com>
2023-07-30 21:58:25 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const user = require('./user');
-const Course = require('./Course');
+const User = require('./User');
+const Thought = require('./Thought');
 
-module.exports = { user, Course };
+module.exports = { User, Thought };
